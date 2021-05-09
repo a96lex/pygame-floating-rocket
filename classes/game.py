a5bbbc1f595908ba4839123403d4eae5b83c59fe
a96lex <a96lex@gmail.com>
2021-05-09 18:12:50 +0200

    new game class

---
+++ classes/game.py
@@ -0,0 +1,100 @@
+from helpers import colors, physics
+from player import Player
+from stars import Stars
+from pipe import Pipe
+
+
+class Game(object):
+    def __init__(self, win):
+        self.win = win
+
+        self.pipe_gap = 350
+        self.pipe_width = 100
+        self.n_pipes = 2
+
+        self.stars = []
+        self.star_layers = 10
+        for i in range(self.star_layers):
+            self.stars.append(
+                Stars(
+                    surface=win,
+                    star_count=int(1000 / self.star_layers),
+                    star_movement=[-(2 + 2 * i / (self.star_layers)), 0],
+                    star_size=2 + i * 2 / self.star_layers,
+                )
+            )
+
+        self.clock_ticks = 0
+        self.highscore = 0
+
+    def init_game(self):
+        self.points = 0
+        self.is_main_loop = True
+        self.collided = False
+        self.player = Player(win)
+        self.pipes = [
+            Pipe(surface=self.win, pipe_gap=self.pipe_gap, pipe_width=self.pipe_width)
+            for i in range(self.n_pipes)
+        ]
+
+        for i in range(self.n_pipes):
+            self.pipes[i].rect1.x += i * (screen_width + 100) / (self.n_pipes)
+            self.pipes[i].rect2.x += i * (screen_width + 100) / (self.n_pipes)
+
+    def main_loop(self):
+        self.win.fill(colors.BACKGROUND)
+
+        for star_layer in self.stars:
+            star_layer.update_and_draw(self.win)
+            star_layer.star_movement = [
+                star_layer.star_movement[0],
+                self.player.vel_y * 0.005 * star_layer.size,
+            ]
+
+        for pipe in self.pipes:
+            pipe.update_and_draw(self.win)
+            if physics.check_collision(self.player, pipe):
+                self.collided = True
+
+            if pipe.rect1.x < 0 - pipe.width:
+                if self.pipe_gap > 130:
+                    self.pipe_gap -= 5
+                if self.pipe_width > 10:
+                    self.pipe_width -= 1.5
+
+                pipe.reset(win, self.pipe_gap, self.pipe_width)
+
+        self.points += (pygame.time.get_ticks() - self.clock_ticks) / 100000
+
+        self.player.draw(self.win)
+
+        text = font.render(f"points: {int(self.points)}", 5, (255, 255, 255))
+        self.win.blit(text, (10, 10))
+
+        self.player.update()
+        pygame.display.update()
+
+    def final_screen(self):
+        self.win.fill(colors.BACKGROUND)
+
+        for star_layer in self.stars:
+            star_layer.update_and_draw(self.win)
+            star_layer.star_movement = [
+                star_layer.star_movement[0],
+                0,
+            ]
+
+        texts = []
+        texts.append(
+            font_big.render(f"Final points: {int(self.points)}", 5, (255, 255, 255))
+        )
+        texts.append(
+            font_big.render(f"Highscore: {int(self.highscore)}", 5, (255, 255, 255))
+        )
+        texts.append(font.render("Press R to restart", 5, (255, 255, 255)))
+
+        text_rect = texts[0].get_rect(center=(screen_width / 2, screen_height / 2))
+
+        for i in range(len(texts)):
+            self.win.blit(texts[i], text_rect.move((0, 100 * (i - len(texts) / 2))))
+        pygame.display.update()
--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import pygame
 import numpy as np
 import sys
-from helpers import colors, physics
-from classes.player import Player
-from classes.stars import Stars
-from classes.pipe import Pipe
 
 
 pygame.init()
@@ -25,127 +21,27 @@ screen_width, screen_height = 1080, 720
 
 win = pygame.display.set_mode((screen_width, screen_height))
 
-stars = []
+game = Game(win=win)
 
-star_layers = 10
-
-for i in range(star_layers):
-    stars.append(
-        Stars(
-            surface=win,
-            star_count=int(1000 / star_layers),
-            star_movement=[-(2 + 2 * i / (star_layers)), 0],
-            star_size=2 + i * 2 / star_layers,
-        )
-    )
-
-
-def init_game():
-    pipe_gap = 350
-    pipe_width = 100
-
-    n_pipes = 2
-
-    player = Player(win)
-    pipes = [
-        Pipe(surface=win, pipe_gap=pipe_gap, pipe_width=pipe_width)
-        for i in range(n_pipes)
-    ]
-
-    for i in range(n_pipes):
-        pipes[i].rect1.x += i * (screen_width + 100) / (n_pipes)
-        pipes[i].rect2.x += i * (screen_width + 100) / (n_pipes)
-
-    points = 0
-
-    return player, pipes, points, pipe_gap, pipe_width
-
-
-def main_loop(points, pipe_gap, pipe_width, clock_ticks):
-    collided = False
-    win.fill(colors.BACKGROUND)
-
-    for star_layer in stars:
-        star_layer.update_and_draw(win)
-        star_layer.star_movement = [
-            star_layer.star_movement[0],
-            player.vel_y * 0.005 * star_layer.size,
-        ]
-
-    for pipe in pipes:
-        pipe.update_and_draw(win)
-        if physics.check_collision(player, pipe):
-            collided = True
-
-        if pipe.rect1.x < 0 - pipe.width:
-            if pipe_gap > 130:
-                pipe_gap -= 5
-            if pipe_width > 10:
-                pipe_width -= 1.5
-
-            pipe.reset(win, pipe_gap, pipe_width)
-
-    points += (pygame.time.get_ticks() - clock_ticks) / 100000
-
-    player.draw(win)
-
-    text = font.render(f"points: {int(points)}", 5, (255, 255, 255))
-    win.blit(text, (10, 10))
-
-    player.update()
-    pygame.display.update()
-    return points, pipe_gap, pipe_width, collided
-
-
-def final_screen(points, highscore):
-    win.fill(colors.BACKGROUND)
-
-    for star_layer in stars:
-        star_layer.update_and_draw(win)
-        star_layer.star_movement = [
-            star_layer.star_movement[0],
-            0,
-        ]
-
-    texts = []
-    texts.append(font_big.render(f"Final points: {int(points)}", 5, (255, 255, 255)))
-    texts.append(font_big.render(f"Highscore: {int(highscore)}", 5, (255, 255, 255)))
-    texts.append(font.render("Press R to restart", 5, (255, 255, 255)))
-
-    text_rect = texts[0].get_rect(center=(screen_width / 2, screen_height / 2))
-
-    for i in range(len(texts)):
-        win.blit(texts[i], text_rect.move((0, 100 * (i - len(texts) / 2))))
-    pygame.display.update()
-
-
-is_main_loop = True
-
-player, pipes, points, pipe_gap, pipe_width = init_game()
-
-highscore = 0
-
-clock_ticks = 0
+game.init_game()
 
 while True:
     pygame.time.delay(40)
 
-    if is_main_loop:
-        points, pipe_gap, pipe_width, collided = main_loop(
-            points, pipe_gap, pipe_width, clock_ticks
-        )
-        if collided:
-            is_main_loop = False
+    if game.is_main_loop:
+        game.main_loop()
+        if game.collided:
+            game.is_main_loop = False
     else:
-        if points > highscore:
-            highscore = points
-        final_screen(points, highscore)
-        key = pygame.key.get_pressed()
+        if game.points > game.highscore:
+            game.highscore = game.points
+        game.final_screen()
 
+        key = pygame.key.get_pressed()
         if key[pygame.K_r]:
-            clock_ticks = pygame.time.get_ticks()
-            player, pipes, points, pipe_gap, pipe_width = init_game()
-            is_main_loop = True
+            game.init_game()
+            game.clock_ticks = pygame.time.get_ticks()
+            game.is_main_loop = True
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
